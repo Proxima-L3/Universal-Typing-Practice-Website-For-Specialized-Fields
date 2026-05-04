aeff11040a07c4befd838b5da8c612c0b75adcd1
Baachen Khan Adelbert <98500706+Proxima-L3@users.noreply.github.com>
2026-05-03 17:04:26 -0700

    Changes to 404.html and main.jsx so subpage refresh functions correctly

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,13 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+// redirect is set to null (falsy) if key does not exist and if it does exist it is set to the value of the key which is a string (truthy).. then key/value is removed from session storage so code doesnt run on a normal page load only when 404.html is loaded on subpage refresh.. then change url without triggering page navigation (react router automatically takes care of page navigation when the url changes to a subpath)
+const redirect = sessionStorage.getItem('redirect');
+if (redirect) {
+  sessionStorage.removeItem('redirect');
+  window.history.replaceState(null, '', redirect);
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename='/Universal-Typing-Practice-Website-For-Specialized-Fields/'>
