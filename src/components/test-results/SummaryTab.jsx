a66bf4c3fbf7da2cff3a6ed6bcad094e280f0746
Baachen Khan Adelbert <98500706+Proxima-L3@users.noreply.github.com>
2026-06-10 15:46:52 -0700

    Added content to display SummaryTab

--- src/components/test-results/SummaryTab.jsx
+++ src/components/test-results/SummaryTab.jsx
@@ -1,8 +1,8 @@
 // import { useState, useRef, useEffect } from 'react';
 
-// import { useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
-import '../App.css';
+import '/src/App.css';
 
 
 function SummaryTab () {
@@ -61,20 +61,123 @@ function SummaryTab () {
                 // showWordCounter,
 
 
-    const displaySummaryTab = function () {
-        // insert test results summary and leaderboard tabs with styling
-        
-        // insert test results Test Summary Title with styling
-        
-        
-        // insert test results summary column one
-        
-        // insert test results summary column two
 
+    const displayTestDataInConsole = function () {
+        console.log('---------------------------------------------------------')
+        console.log('---------------------------------------------------------')
+        console.log('entryId:' + entryId)
+        console.log('dateTime:' + testDateTimeTaken)
+        console.log('kps:' + typingSpeedKPS)
+        console.log('kph:' + typingSpeedKPH)
+        console.log('cpm:' + typingSpeedCPM)
+        console.log('wpm:' + typingSpeedWPM)
+        console.log('accuracy:' + typingAccuracy)
+        console.log('words completed:' + testWordsCompleted)
+        console.log('time completed in:' + testTimeCompletedIn)
+        console.log('basic test option:' + basicTestOption)
+        console.log('custom test bool:' + customTestBool)
+        console.log('test type:' + testType)
+        console.log('test time limit:' + timeLimit)
+        console.log('test word count:' + wordCount)
+        console.log('custom time bool:' + customTimeBool)
+        console.log('custom text bool:' + customTextBool)
+        console.log('autoGenModifiers:', autoGenModifiers)
+        console.log('specialization field:' + specializationField)
+        console.log('insertion point style:' + insertionPointStyle)
+        console.log('show insertion point:' + showInsertionPoint)
+        console.log('show stats:' + showStats)
+        console.log('show timer:' + showTimer)
+        console.log('show word counter:' + showWordCounter)
+        console.log('---------------------------------------------------------')
+        console.log('---------------------------------------------------------')
+    }
+
+
+    const displaySummaryTab = function () {
+
+        const date = new Date(testDateTimeTaken);
+        const dateFormatted = `${String(date.getDate()).padStart(2, '0')}-${String(date.getMonth() + 1).padStart(2,'0')}-${date.getFullYear()} / @ ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2,'0')}`;
+
+        return (
+            <>
+                {/* // insert test results summary and leaderboard tabs with styling */}
+
+                <div className='summaryRows'>
+                    <div className='genericSectionTitle summaryTitle'>Test Results Summary</div>
+                </div>
+
+                <div className='summaryRows'>
+                    <div className='summaryColumns'>
+
+                        <div className='summaryColumnOne'>
+                            <div className='statRow'>
+                                <span className='statLabel'>Test Type: </span>
+                                <span className='statValue'>{customTestBool ? 'Custom - ' : 'Basic Preset - '}{customTestBool ? testType : basicTestOption}</span>
+                            </div>
+                            <div className='statRow'>
+                                <span className='statLabel'>Date/Time Taken: </span>
+                                <span className='statValue'>{dateFormatted}</span>
+                            </div>
+                            <div className='statRow'>
+                                <span className='statLabel'>Text Filtering Modifiers Chosen: </span>
+                                <span className='statValue'>{Object.entries(autoGenModifiers).filter(([key, val]) => val).map(([key]) => key).join(', ')}</span>
+                            </div>
+                            <div className='statRow'>
+                                <span className='statLabel'>Specialization Field: </span>
+                                <span className='statValue'>{specializationField}</span>
+                            </div>
+                            <div className='statRow'>
+                                <span className='statLabel'>{testType == 'Timer Based' || basicTestOption == 'timed' ? 'Time Limit Chosen' : 'Word-Count Chosen'}: </span>
+                                <span className='statValue'>{testType == 'Timer Based' || basicTestOption == 'timed' ? timeLimit : wordCount} Seconds</span>
+                            </div>
+                            <div className='statRow'>
+                                <span className='statLabel'>Words Completed: </span>
+                                <span className='statValue'>{testWordsCompleted} Words</span>
+                            </div>
+                            <div className='statRow'>
+                                <span className='statLabel'>Time Completed in: </span>
+                                <span className='statValue'>{testTimeCompletedIn} Secs</span>
+                            </div>
+                            <div className='statRow'>
+                                <span className='statLabel'>WPM: </span>
+                                <span className='statValue'>{typingSpeedWPM}</span>
+                            </div>
+                            <div className='statRow'>
+                                <span className='statLabel'>CPM: </span>
+                                <span className='statValue'>{typingSpeedCPM}</span>
+                            </div>
+                            <div className='statRow'>
+                                <span className='statLabel'>KPS: </span>
+                                <span className='statValue'>{typingSpeedKPS}</span>
+                            </div>
+                            <div className='statRow'>
+                                <span className='statLabel'>KPH: </span>
+                                <span className='statValue'>{typingSpeedKPH}</span>
+                            </div>
+                            <div className='statRow'>
+                                <span className='statLabel'>Chars Typed Correctly: </span>
+                                <span className='statValue'>{Math.floor(typingAccuracy)}%</span>
+                            </div>
+                        </div>
+
+                        <div className='summaryColumnTwo'>
+                            <div className='summaryColumnTwoButtons'>
+                                <div>
+                                    <input type='username' />
+                                    <button></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
     }
 
     return (
         <>
+
+            {displayTestDataInConsole()}
             <div className='testResultsSummaryTab'>{displaySummaryTab()}</div>
         </>
     )
