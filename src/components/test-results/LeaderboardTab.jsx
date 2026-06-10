9f945afeb71ec117055f9f626cd105740476b02c
Baachen Khan Adelbert <98500706+Proxima-L3@users.noreply.github.com>
2026-06-10 15:49:20 -0700

    Added code in TestResultsPage to help navigate to TestResultsPage to conditionally render summary tab or leaderboard tab depending on what user clicks

--- src/components/test-results/LeaderboardTab.jsx
+++ src/components/test-results/LeaderboardTab.jsx
@@ -2,7 +2,7 @@
 
 // import { useLocation } from 'react-router-dom';
 
-import '../App.css';
+import '/src/App.css';
 
 
 function LeaderboardTab () {
--- src/pages/TestResultsPage.jsx
+++ src/pages/TestResultsPage.jsx
@@ -3,16 +3,18 @@ import { useState, useRef, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 // import QuickLinkCard from "../components/common/QuickLinkCard";
-import SummaryTab from '../components/test-results/SummaryTab';
-import LeaderboardTab from '../components/test-results/LeaderboardTab';
+import SummaryTab from '/src/components/test-results/SummaryTab.jsx';
+import LeaderboardTab from '/src/components/test-results/LeaderboardTab.jsx';
 
-import '../App.css';
+import '/src/App.css';
 
 
 function TestResultsPage () {
-
+    
+    const [activeTab, setActiveTab] = useState('summary');
+    
     const snapShotData = JSON.parse(sessionStorage.getItem('typingPracticeFieldSnapshot'));
-
+    
     const typingPracFieldSnapshot = snapShotData.userText.map((item, i) => (
         <span className={item.className} key={i}>{item.char}</span>
     )).concat(snapShotData.practiceText.slice(snapShotData.userText.length).map((char, i) => (
@@ -21,43 +23,13 @@ function TestResultsPage () {
     
     // a useRef & useEffect statement that automatically scrolls past the test snapshot to the test result section of the page on page load
     const testSectionRef = useRef(null);
-
+    
     useEffect(() => {
-        if (testSectionRef != null) {
+        if (testSectionRef.current != null) {
             testSectionRef.current.scrollIntoView({ behavior: 'smooth' });
         }
     }, []);
-
-    const displayTestDataInConsole = function () {
-        console.log('---------------------------------------------------------')
-        console.log('---------------------------------------------------------')
-        console.log('entryId:' + entryId)
-        console.log('dateTime:' + testDateTimeTaken)
-        console.log('kps:' + typingSpeedKPS)
-        console.log('kph:' + typingSpeedKPH)
-        console.log('cpm:' + typingSpeedCPM)
-        console.log('wpm:' + typingSpeedWPM)
-        console.log('accuracy:' + typingAccuracy)
-        console.log('words completed:' + testWordsCompleted)
-        console.log('time completed in:' + testTimeCompletedIn)
-        console.log('basic test option:' + basicTestOption)
-        console.log('custom test bool:' + customTestBool)
-        console.log('test type:' + testType)
-        console.log('test time limit:' + timeLimit)
-        console.log('test word count:' + wordCount)
-        console.log('custom time bool:' + customTimeBool)
-        console.log('custom text bool:' + customTextBool)
-        console.log('autoGenModifiers:', autoGenModifiers)
-        console.log('specialization field:' + specializationField)
-        console.log('insertion point style:' + insertionPointStyle)
-        console.log('show insertion point:' + showInsertionPoint)
-        console.log('show stats:' + showStats)
-        console.log('show timer:' + showTimer)
-        console.log('show word counter:' + showWordCounter)
-        console.log('---------------------------------------------------------')
-        console.log('---------------------------------------------------------')
-    }
-
+    
     const displayTypingPracFieldSnapshot = function () {
         return (
             <>
@@ -65,17 +37,36 @@ function TestResultsPage () {
             </>
         )
     }
-
+    
+    const displayActiveTab = function () {
+        console.log(activeTab)
+        if (activeTab == 'summary') {
+            return (
+                <SummaryTab></SummaryTab>
+            )
+        }
+        else if (activeTab == 'leaderboard'){
+            return (
+                <LeaderboardTab></LeaderboardTab>
+            )
+        }
+        
+        // return (
+            //     <>
+            
+            //     <SummaryTab></SummaryTab>
+            //     <div className={`typingStats ${ timerExpired || wordCountReached ? 'typingStatsTestEnded' : 'typingStatsTestRunning'} ${showStats ? '' : 'contentHidden'}`} tabIndex='3'>{displayStats()}</div>
+            //     </>
+            // )
+        }
+        
     return (
         <>
-            <div>{displayTestDataInConsole()}</div>
             <div className='testResultsTestSnapshot'>{displayTypingPracFieldSnapshot()}</div>
             <hr className='sectionDividerBottom'/>
-
             <div className='testResultsSectionTitle' ref={testSectionRef}>Test Results</div>
             <hr className='sectionDividerTop'/>
-            <SummaryTab></SummaryTab>
-            {/* <LeaderboardTab></LeaderboardTab> */}
+            <div>{displayActiveTab()}</div>
         </>
     )
 }
